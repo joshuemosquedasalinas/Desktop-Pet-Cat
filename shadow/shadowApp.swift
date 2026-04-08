c7979cd745a973cf66ff872ac4b5f3431d052bd0
Joshue Mosqueda Salinas <joshuemosquedasalinas@gmail.com>
2026-04-08 11:00:35 -0600

    Remove OpenRouter logic

--- shadow/shadowApp.swift
+++ shadow/shadowApp.swift
@@ -9,12 +9,9 @@ import SwiftUI
 
 @main
 struct shadowApp: App {
-    @StateObject private var model = CatViewModel()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(model)
                 .frame(minWidth: 220, minHeight: 220)
         }
         .defaultSize(width: 360, height: 360)
